77f89c6e7ece4d87a97b12905046cb7d707951b9
wenbo.zhangw <wenbo.zhangw@tongdun.net>
2021-04-30 13:19:50 +0800

    package pom

--- sso-springboot-app/src/main/java/org/example/sso/web/OkController.java
+++ sso-springboot-app/src/main/java/org/example/sso/web/OkController.java
@@ -3,6 +3,9 @@ package org.example.sso.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author ：wenbo.zhangw
  * @date ：Created in 2021/4/30 10:18 上午
@@ -12,7 +15,15 @@ public class OkController {
 
     @RequestMapping("/ok")
     public String ok() {
-
         return "ok";
+    }
+
+    @RequestMapping("/monitor")
+    public Map<String, String> monitor(String education, String house, String fund) {
+        Map<String, String> res = new HashMap<>(3);
+        res.put("education", education);
+        res.put("house", house);
+        res.put("fund", fund);
+        return res;
     }
 }
